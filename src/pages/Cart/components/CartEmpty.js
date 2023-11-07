157aa8c930afe290b258150f9b86446eeecd3435
Humberto Santos <1201hs@gmail.com>
2023-11-07 08:50:49 -0600

    change text

--- src/pages/Cart/components/CartEmpty.js
+++ src/pages/Cart/components/CartEmpty.js
@@ -6,7 +6,7 @@ export const CartEmpty = () => {
         <div className="my-5">
             <p className="bi bi-cart text-green-600 text-7xl mb-5"></p>
             <p>Oops! Your cart looks empty!</p>
-            <p>Add eBooks to your cart from our store collection.</p>
+            <p>Add recipes to your cart.</p>
         </div>
         <Link to="/products" type="button" className="text-white bg-blue-700 hover:bg-blue-800 rounded-lg text-lg px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none">Continue Shopping <i className="ml-2 bi bi-cart"></i></Link>
     </section>
